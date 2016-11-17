4aa59d1f0b31b2245b015bf6df1dfc8bd1f8c015
salqadri <salqadri@gmail.com>
2016-11-16 20:34:52 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-# homebridge-fireswitch
+# homebridge-firegarage
 Homebridge plugin to set/get values in Firebase.
 
 ## Installation
 
-npm install -g homebridge-fireswitch
+npm install -g homebridge-firegarage
 
 ## Configuration
 
 Add this to your '~/.homebridge/config.json' as an accessory:
 ```
 {
-    "accessory": "FireSwitch",
-    "name": "My Switch",
+    "accessory": "FireGarage",
+    "name": "My Garage",
     "server": "https://myserver.firebaseio.com",
-    "path": "/path/to/my/on_off/value",
-    "on_value": "turn_it_on",
-    "off_value": "turn_it_off",
+    "target_state_path": "/path/to/my/taget_state/value",
+    "current_state_path": "/path/to/my/current_state/value",
+    "trigger_path": "/path/to/my/trigger/value",
     "auth_method": "password",
     "auth_credentials": {
         "email": "email@example.com",
@@ -24,9 +24,9 @@ Add this to your '~/.homebridge/config.json' as an accessory:
 }
 ```
 * server: This is the database server hostname assigned to your Firebase instance.
-* path: This is the path in the database reference. This can use [variable expansion](#variables).
-* on_value: This is the value to set when the switch is turned on. If this is blank "on" events are ignored.
-* off_value: This is the value to set when the switch is turned off. If this is blank "off" events are ignored.
+* target_state_path: This is the path in the database to set the target state of the garage. If the target state is open, the value will be 0, and if the target state is to be closed, the value will be 1. This can use [variable expansion](#variables).
+* current_state_path: This is the path in the database to read the current state of the garage. The state must be either 0 to represent the garage being open, or 1 to represent the garage being closed. This can use [variable expansion](#variables).
+* trigger_path: This is the path in the database to a trigger field. Whenever the garage state needs to be changed, this trigger value will be set to 1 for 1 second. This can use [variable expansion](#variables).
 * auth_method: This is the authentication method used to authenticate against the Firebase instance. Options are:
     * "password" to use [authWithPassword()](https://www.firebase.com/docs/web/api/firebase/authwithpassword.html)
     * "customtoken" to use [authWithCustomToken()](https://www.firebase.com/docs/web/api/firebase/authwithcustomtoken.html)
@@ -46,12 +46,6 @@ Variables are expanded after authorization is complete.
 ##### Supported Variables
 * {$uid} - The result of firebase_database.getAuth().uid
 
-## Tutorial
-The project [wiki](https://github.com/mlaanderson/homebridge-fireswitch/wiki/) has
-a [simple tutorial](https://github.com/mlaanderson/homebridge-fireswitch/wiki/Tutorial)
-that demonstrates how to create and control a widget with this project.
-    
 ## Notes
 ##### Currently this works only with Firebase libraries < 3.0.0 
 See [this conversation](https://groups.google.com/forum/#!searchin/firebase-talk/Firebase$203$20Auth$20as$20a$20Client$20on$20NodeJS/firebase-talk/_6Rhro3zBbk/3sTvmPKwGgAJ) in the Firebase Google Groups. For Firebase >= 3.0.0 in NodeJS only custom token authentication is supported. However, the 2.4.1 libraries for NodeJS work well with Firebase 3.x.
-
--- index.js
+++ index.js
@@ -1,44 +1,38 @@
 const path_RE = /\{\$[^${]+\}/g;
-var Service, Characteristic;
+var Service, Characteristic, DoorState;
 var Firebase = require('firebase');
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
+    DoorState = homebridge.hap.Characteristic.CurrentDoorState;
     
-    homebridge.registerAccessory("homebridge-fireswitch", "FireSwitch", fireSwitch);
+    homebridge.registerAccessory("homebridge-firegarage", "FireGarage", fireGarage);
 };
 
-class fireSwitch {
+class fireGarage {
     constructor(log, config) {
         var self = this;
         this.log = log;
         this.name = config.name;
         this.server = config.server;
-        this.path = config.path;
-        this.on_value = config.on_value;
-        this.off_value = config.off_value;
+        this.target_state_path = config.target_state_path;
+        this.current_state_path = config.current_state_path;
+        this.trigger_path = config.trigger_path;
         this.auth_method = config.auth_method;
         this.auth_credentials = config.auth_credentials;
         this._state = false;
         
         this._db = new Firebase(this.server);
-        
-        if (path_RE.test(this.path) == false) {
-            // path doesn't need to get UID or other
-            // vars to expand
-            this._db_path = this._db.child(this.path);
-        }
-        
+
         this._db.onAuth(function(authData) {
             if (authData) {
                 // parse the path
-                self.path = self.path.replace("{$uid}", self._db.getAuth().uid);
-                
-                // have to set the path here since it required
-                // variable expansion after authentication
-                if (path_RE.test(self.path) == true) {
-                    self._db_path = self._db.child(self.path);
+                var uid = self._db.getAuth().uid;
+                var paths = [this.target_state_path, this.current_state_path, this.trigger_path];
+                for (var i = 0; i < paths.length; i++) {
+                    //Do something
+                    paths[i] = paths[i].replace("{$uid}", uid);
                 }
             } else {
                 // try to authorize
@@ -51,74 +45,61 @@ class fireSwitch {
     _authorize() {
         switch (this.auth_method) {
             case 'password':
+                this.log("firegarage: authWithPassword");
                 this._db.authWithPassword(this.auth_credentials);
                 break;
             case 'customtoken':
+                this.log("firegarage: authWithCustomToken");
                 this._db.authWithCustomToken(this.auth_credentials);
                 break;
             case 'anonymously':
+                this.log("firegarage: authAnonymously");
                 this._db.authAnonymously();
                 break;
         }
     }
-    
-    getState(callback) {
-        var self = this;
-        this._db_path.on('value', function(snapshot) {
-            var val = snapshot.val();
-            if ((self.on_value) && (self.on_value == val)) {
-                self._state = true;
-                callback(null, self._state);
-                return;
-            }
-            
-            if ((self.off_value) && (self.off_value == val)) {
-                self._state = false;
-                callback(null, self._state);
-                return;
-            }
-            
-            if ((self.on_value == undefined) || (self.on_value == null)) {
-                self.state = (val == undefined) || (val == null);
-                callback(null, self._state);
-                return;
-            }
-            
-            if ((self.off_value == undefined) || (self.off_value == null)) {
-                self.state = (val != undefined) && (val != null);
-                callback(null, self._state);
-                return;
-            }
+
+    getTargetState(callback) {
+        this.log("firegarage: getTargetState requested");
+        this._db.child(this.target_state_path).once("value", function(snapshot) {
+            callback(null, snapshot.val());
         });
-        callback(null, this._state);
+    }
+
+    getCurrentState(callback) {
+        this.log("firegarage: getCurrentState requested");
+        this._db.child(this.current_state_path).once("value", function(snapshot) {
+            callback(null, snapshot.val());
+        });
     }
     
-    setState(val, callback) {
-        var self = this;
-        this._state = val;
-        if ((val == true) && (this.on_value)) {
-            this._db_path.set(this.on_value).then(function() {
-                callback(null, self._state);
-            });
-        } else if ((val == false) && (this.off_value)) {
-            this._db_path.set(this.off_value).then(function() {
-                callback(null, self._state);
-            });
-        }
+    setTargetState(val, callback) {
+        this.log("firegarage: setTargetState " + val);
+        this._db.child(this.target_state_path).set(val).then(function() {
+            callback(null, val);
+        })
     }
     
     identify(callback) {
-        this.log("Identify requested");
+        this.log("firegarage: Identify requested");
         callback();
     }
     
     getServices() {
-        var switchService = new Service.Switch(this.name);
-        
-        switchService
-            .getCharacteristic(Characteristic.On)
-            .on('set', this.setState.bind(this));
+        this.log("firegarage: getServices requested");
+        this.garageDoorOpener = new Service.GarageDoorOpener(this.name,this.name);
+        this.currentDoorState = this.garageDoorOpener.getCharacteristic(DoorState);
+        this.currentDoorState.on('get', this.getCurrentState.bind(this))
+        this.targetDoorState = this.garageDoorOpener.getCharacteristic(Characteristic.TargetDoorState);
+        this.targetDoorState.on('set', this.setTargetState.bind(this));
+        this.targetDoorState.on('get', this.getTargetState.bind(this));
+
+        this.infoService = new Service.AccessoryInformation();
+        this.infoService
+          .setCharacteristic(Characteristic.Manufacturer, "Opensource Community")
+          .setCharacteristic(Characteristic.Model, "Firebase GarageDoorOpener")
+          .setCharacteristic(Characteristic.SerialNumber, "Version 1.0.0");
             
-        return [switchService];
+        return [this.garageDoorOpener, this.infoService];
     }
 }
