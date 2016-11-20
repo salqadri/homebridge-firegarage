a5be22496f53b43fc760c76a79e03f7a95bcf09e
salqadri <salqadri@gmail.com>
2016-11-19 21:39:27 -0800

    Upgrade to Firebase 3

--- README.md
+++ README.md
@@ -5,14 +5,22 @@ Homebridge plugin to set/get values in Firebase.
 
 npm install -g homebridge-firegarage
 
-## Configuration
+## Firebase Setup
+* Setup a project at https://console.firebase.google.com
+* Click "Add another App"
+* Choose "Web"
+* Copy the generated values for apiKey, authDomain and databaseURL
+
+## Homebridge Configuration
 
 Add this to your '~/.homebridge/config.json' as an accessory:
 ```
 {
     "accessory": "FireGarage",
     "name": "My Garage",
+    "auth_domain": "myserver.firebaseio.com",
     "server": "https://myserver.firebaseio.com",
+    "api_key": "R3plac3ThisWithY0urAP1KeyFr0mTh3Consol3"
     "target_state_path": "/path/to/my/taget_state/value",
     "current_state_path": "/path/to/my/current_state/value",
     "trigger_path": "/path/to/my/trigger/value",
@@ -23,14 +31,16 @@ Add this to your '~/.homebridge/config.json' as an accessory:
     }
 }
 ```
-* server: This is the database server hostname assigned to your Firebase instance.
+* auth_domain: The auth domain assigned to your Firebase instance
+* server: This is the database server hostname assigned to your Firebase instance
+* api_key: The API Key of your Firebase project
 * target_state_path: This is the path in the database to set the target state of the garage. If the target state is open, the value will be 0, and if the target state is to be closed, the value will be 1. This can use [variable expansion](#variables).
 * current_state_path: This is the path in the database to read the current state of the garage. The state must be either 0 to represent the garage being open, or 1 to represent the garage being closed. This can use [variable expansion](#variables).
 * trigger_path: This is the path in the database to a trigger field. Whenever the garage state needs to be changed, this trigger value will be set to 1 for 1 second. This can use [variable expansion](#variables).
 * auth_method: This is the authentication method used to authenticate against the Firebase instance. Options are:
-    * "password" to use [authWithPassword()](https://www.firebase.com/docs/web/api/firebase/authwithpassword.html)
-    * "customtoken" to use [authWithCustomToken()](https://www.firebase.com/docs/web/api/firebase/authwithcustomtoken.html)
-    * "anonymously" to use [authAnonymously()](https://www.firebase.com/docs/web/api/firebase/authanonymously.html)
+    * "password" to use [signInWithEmailAndPassword()](https://firebase.google.com/docs/auth/web/password-auth)
+    * "customtoken" to use [signInWithCustomToken()](https://firebase.google.com/docs/auth/web/custom-auth)
+    * "anonymously" to use [signInAnonymously()](https://firebase.google.com/docs/auth/web/anonymous-auth)
 * auth_credentials: This is the authentication credentials to use with auth_method. For:
     *  "password" this should be a JSON object containing the email and password values.
     *  "customtoken" this should be a string containing a token generated in the Firebase console.
@@ -45,7 +55,3 @@ Variables in the path are set in curly braces like:
 Variables are expanded after authorization is complete.
 ##### Supported Variables
 * {$uid} - The result of firebase_database.getAuth().uid
-
-## Notes
-##### Currently this works only with Firebase libraries < 3.0.0 
-See [this conversation](https://groups.google.com/forum/#!searchin/firebase-talk/Firebase$203$20Auth$20as$20a$20Client$20on$20NodeJS/firebase-talk/_6Rhro3zBbk/3sTvmPKwGgAJ) in the Firebase Google Groups. For Firebase >= 3.0.0 in NodeJS only custom token authentication is supported. However, the 2.4.1 libraries for NodeJS work well with Firebase 3.x.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path_RE = /\{\$[^${]+\}/g;
 var Service, Characteristic, DoorState;
-var Firebase = require('firebase');
+var firebase = require('firebase');
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
@@ -16,6 +16,8 @@ class fireGarage {
         this.log = log;
         this.name = config.name;
         this.server = config.server;
+        this.auth_domain = config.auth_domain;
+        this.api_key = config.api_key;
         this.target_state_path = config.target_state_path;
         this.current_state_path = config.current_state_path;
         this.trigger_path = config.trigger_path;
@@ -23,38 +25,61 @@ class fireGarage {
         this.auth_credentials = config.auth_credentials;
         this._state = false;
         
-        this._db = new Firebase(this.server);
+        var config = {
+            apiKey: this.api_key,
+            authDomain: this.auth_domain,
+            databaseURL: this.server
+        };
+        firebase.initializeApp(config);
+        this._db = firebase.database();
+        this._auth = firebase.auth();
 
-        this._db.onAuth(function(authData) {
-            if (authData) {
-                // parse the path
-                var uid = self._db.getAuth().uid;
-                var paths = [self.target_state_path, self.current_state_path, self.trigger_path];
-                for (var i = 0; i < paths.length; i++) {
-                    //Do something
-                    paths[i] = paths[i].replace("{$uid}", uid);
-                }
-            } else {
-                // try to authorize
-                self._authorize();
+        this._auth.onAuthStateChanged(function(user) {
+          if (user) {
+            // User signed in!
+            var uid = user.uid;
+            self.log("firegarage: Authenticated");
+            var paths = [self.target_state_path, self.current_state_path, self.trigger_path];
+            for (var i = 0; i < paths.length; i++) {
+                //Do something
+                paths[i] = paths[i].replace("{$uid}", uid);
             }
+          } else {
+            // User logged out
+            self._authorize();
+          }
         });
-        
     }
     
     _authorize() {
+        var self = this;
         switch (this.auth_method) {
             case 'password':
-                this.log("firegarage: authWithPassword");
-                this._db.authWithPassword(this.auth_credentials);
+                self.log("firegarage: authWithPassword");
+                self._auth.signInWithEmailAndPassword(self.auth_credentials["email"], self.auth_credentials["password"]).catch(function(error) {
+                  // Handle Errors here.
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  self.log("Auth Error " + errorCode + ": " + error.message);
+                });
                 break;
             case 'customtoken':
-                this.log("firegarage: authWithCustomToken");
-                this._db.authWithCustomToken(this.auth_credentials);
+                self.log("firegarage: authWithCustomToken");
+                firebase.auth().signInWithCustomToken(self.auth_credentials).catch(function(error) {
+                  // Handle Errors here.
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  self.log("Auth Error " + errorCode + ": " + error.message);
+                });
                 break;
             case 'anonymously':
-                this.log("firegarage: authAnonymously");
-                this._db.authAnonymously();
+                self.log("firegarage: authAnonymously");
+                firebase.auth().signInAnonymously().catch(function(error) {
+                  // Handle Errors here.
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  self.log("Auth Error " + errorCode + ": " + error.message);
+                });
                 break;
         }
     }
